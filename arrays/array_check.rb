d37636ccd062c5223d8f2bac443c7c263cd61892
Muthhukumar Palaniyapan <palaniyapan@gmail.com>
2017-12-20 23:01:45 -0500

    arrays

---
+++ arrays/array_check.rb
@@ -0,0 +1,21 @@
+#array_check.rb 
+
+#check if an element is present in an array 
+
+
+arr = [1,3,5,7,9,11]
+
+
+
+def checker(arr,num)
+
+	present = arr.include?num 
+	puts present 
+
+
+end 
+
+
+checker(arr,number)
+
+
---
+++ arrays/array_transformer.rb
@@ -0,0 +1,12 @@
+#array_tranformer.rb 
+
+arr1 = [1,2,3,4,5,6,7,8,9]
+
+arr2 = arr1.collect{|value| value * 2}
+
+p arr1 
+
+p "Printing the next array"
+
+p arr2
+
